4f476827999dcef9ace6854d450ee5600bc73637
nyaxt <ueno@nyaxtstep.com>
2013-06-17 15:31:00 +0900

    test signal gen

--- spdif_dai_t.v
+++ spdif_dai_t.v
@@ -25,14 +25,110 @@ endtask
 
 task recv_B;
     begin
-        recv_rawbit(1);
-        recv_rawbit(1);
-        recv_rawbit(1);
-        recv_rawbit(0);
-        recv_rawbit(1);
-        recv_rawbit(0);
-        recv_rawbit(0);
-        recv_rawbit(0);
+        if(signal) begin
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(1);
+        end else begin
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(0);
+        end
+    end
+endtask
+
+task recv_M;
+    begin
+        if(signal) begin
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(1);
+        end else begin
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(0);
+        end
+    end
+endtask
+
+task recv_W;
+    begin
+        if(signal) begin
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(1);
+        end else begin
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(0);
+            recv_rawbit(1);
+            recv_rawbit(0);
+            recv_rawbit(0);
+        end
+    end
+endtask
+
+task recv_bmcbit;
+    input b;
+    begin
+        if(signal) begin
+            if(b) begin
+                recv_rawbit(0);
+                recv_rawbit(1);
+            end else begin
+                recv_rawbit(0);
+                recv_rawbit(0);
+            end
+        end else begin
+            if(b) begin
+                recv_rawbit(1);
+                recv_rawbit(0);
+            end else begin
+                recv_rawbit(1);
+                recv_rawbit(1);
+            end
+        end
+    end
+endtask
+
+task recv_bmcbyte;
+    input [7:0] byte;
+    begin
+        recv_bmcbit(byte[7]);
+        recv_bmcbit(byte[6]);
+        recv_bmcbit(byte[5]);
+        recv_bmcbit(byte[4]);
+        recv_bmcbit(byte[3]);
+        recv_bmcbit(byte[2]);
+        recv_bmcbit(byte[1]);
+        recv_bmcbit(byte[0]);
     end
 endtask
 
@@ -51,6 +147,13 @@ initial begin
 	#(TCLK*3);
 
     recv_B();
+    recv_bmcbyte(8'h00);
+    recv_bmcbyte(8'h00);
+    recv_bmcbyte(8'h00);
+    recv_bmcbit(1);
+    recv_bmcbit(1);
+    recv_bmcbit(1);
+    recv_bmcbit(1);
 
 	#(TCLK*100000);
 	// #(1000_000_00);
