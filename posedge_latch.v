dcbdf5329e7807ff079dd45412ee9f7659298aa4
nyaxt <ueno@nyaxtstep.com>
2013-06-23 03:25:23 +0900

    posedge_latch

--- posedge_latch.v
+++ posedge_latch.v
@@ -1,40 +1,21 @@
-module ringbuf(
+module posedge_latch(
     input clk,
-    input rst,
 
-    input [23:0] data_i,
     input wpulse_i,
-    
-    input pop_i,
-    input [3:0] offset_i,
-    output [23:0] data_o);
+    output pop_o);
 
-reg [23:0] mem [15:0];
-
-// WRITE
 reg wpulse_handled_ff;
-reg [3:0] witer;
-always @(posedge clk) begin
-    if(rst) begin
-        witer <= 0;
-    end else if(wpulse_i && !wpulse_handled_ff) begin
+reg pop_ff;
+assign pop_o = pop_ff;
+always @(negedge clk) begin
+    pop_ff <= 0;
+
+    if(wpulse_i && !wpulse_handled_ff) begin
         wpulse_handled_ff <= 1;
-        mem[witer] <= data_i;
-        witer <= witer + 1;
+        pop_ff <= 1; 
     end else if(!wpulse_i)
         wpulse_handled_ff <= 0;
 end
 
-// READ
-reg [3:0] riter;
-wire [3:0] raddr = riter - offset_i;
-assign data_o = mem[raddr];
-
-always @(posedge clk) begin
-    if(rst)
-        riter <= 15;
-    else if(pop_i)
-        riter <= riter + 1; 
-end
 
 endmodule
--- posedge_latch_t.v
+++ posedge_latch_t.v
@@ -6,95 +6,28 @@ module ringbuf_t;
 reg clk;
 reg rst;
 
-reg [23:0] data_i;
 reg wpulse_i;
 
-reg pop_i;
-reg [3:0] offset_i;
-
-ringbuf uut(
-    .clk(clk), .rst(rst),
-    .data_i(data_i), .wpulse_i(wpulse_i),
-    .pop_i(pop_i), .offset_i(offset_i));
+posedge_latch uut(.clk(clk), .wpulse_i(wpulse_i));
 
 parameter TCLK = 41.0; // ~40.69ns (24.576Mhz)
 
-task recv_data;
-    input [23:0] b;
-    begin
-        data_i = b;
-        #10;
-        wpulse_i = 1;
-        #130;
-        wpulse_i = 0;
-        #500;
-    end
-endtask
-
 initial begin
-	$dumpfile("ringbuf_t.lxt");
+	$dumpfile("posedge_latch_t.lxt");
 	$dumpvars(0, ringbuf_t);
 	
 	clk = 1'b0;
-
-	data_i = 24'h0;
     wpulse_i = 0;
-    pop_i = 0;
-    offset_i = 0;
 
-	rst = 1'b0;
-	#(TCLK*6);
-	rst = 1'b1;
-	#TCLK;
-	rst = 1'b0;
-	#TCLK;
-
-    recv_data(24'h0);
-    recv_data(24'h1);
-    recv_data(24'h2);
-    recv_data(24'h3);
-    recv_data(24'h4);
-    recv_data(24'h5);
-    recv_data(24'h6);
-    recv_data(24'h7);
-    recv_data(24'h8);
-    recv_data(24'h9);
-    recv_data(24'ha);
-    recv_data(24'hb);
-    recv_data(24'hc);
-    recv_data(24'hd);
-    recv_data(24'he);
-    recv_data(24'hf);
-
-    offset_i = 0;
-    #TCLK;
-    $display("15 = %d", uut.data_o);
-    offset_i = 1;
-    #TCLK;
-    $display("14 = %d", uut.data_o);
-    offset_i = 2;
-    #TCLK;
-    $display("13 = %d", uut.data_o);
-    offset_i = 3;
-    #TCLK;
-    $display("12 = %d", uut.data_o);
-
-    pop_i = 1;
-    #TCLK;
-    pop_i = 0;
-
-    offset_i = 0;
-    #TCLK;
-    $display("0 = %d", uut.data_o);
-    offset_i = 1;
-    #TCLK;
-    $display("15 = %d", uut.data_o);
-    offset_i = 2;
-    #TCLK;
-    $display("14 = %d", uut.data_o);
-    offset_i = 3;
-    #TCLK;
-    $display("13 = %d", uut.data_o);
+    #100;
+    wpulse_i = 1;
+    #100;
+    wpulse_i = 0;
+    #100;
+    wpulse_i = 1;
+    #30;
+    wpulse_i = 0;
+    #100;
 
 	$finish(2);
 end
