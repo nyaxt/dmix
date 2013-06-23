2a899ae97c2f9ff0d58df172adc51b961a1178c0
nyaxt <ueno@nyaxtstep.com>
2013-06-23 16:00:24 +0900

    match real ip

--- mpemu.v
+++ mpemu.v
@@ -8,15 +8,16 @@ module mpemu(
     input signed [15:0] mplier_i,
     output signed [23:0] mprod_o);
 
-reg signed [23:0] delay[3:0];
-assign mprod_o = delay[3];
+reg signed [23:0] delay[4:0];
+assign mprod_o = delay[4];
 
-wire [23:0] prod = (mpcand_i * mplier_i) >>> 15;
+wire [23:0] prod = (mpcand_i * mplier_i) >>> 16;
 always @(posedge clk) begin
     delay[0] <= prod;
     delay[1] <= delay[0];
     delay[2] <= delay[1];
     delay[3] <= delay[2];
+    delay[4] <= delay[3];
 end
 
 endmodule
