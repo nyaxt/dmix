b9be9b16ffbaf3ed44a8e18eff1b1d0eed97b4b6
nyaxt <ueno@nyaxtstep.com>
2015-01-12 15:37:30 +0900

    varclk lock to specific clk_per_hafblit

--- spdif_dai_varclk.v
+++ spdif_dai_varclk.v
@@ -23,7 +23,7 @@ wire [(MAX_CLK_PER_HALFBIT_LOG2-1):0] clk_per_halfbit;
 wire locked;
 spdif_dai #(
 	.MAX_CLK_PER_HALFBIT_LOG2(MAX_CLK_PER_HALFBIT_LOG2)
-) dai(
+) dai_fixedclk(
     .clk(clk),
     .rst(rst),
 
@@ -43,8 +43,8 @@ always @(posedge clk)
     last_locked_ff <= locked;
 assign rst_o = !last_locked_ff && locked;
 
-reg [11:0] unlocked_duration_counter;
-wire unlocked_for_longtime = unlocked_duration_counter == 12'hfff;
+reg [13:0] unlocked_duration_counter;
+wire unlocked_for_longtime = unlocked_duration_counter == 14'h3fff;
 always @(posedge clk) begin
 	if(rst || locked)
 		unlocked_duration_counter <= 0;
@@ -52,65 +52,42 @@ always @(posedge clk) begin
 		unlocked_duration_counter <= unlocked_duration_counter + 1;
 end
 
-reg [(MAX_CLK_PER_HALFBIT_LOG2-1):0] clk_per_halfbit_ff;
-always @(posedge clk) begin
-	if(rst)
-		clk_per_halfbit_ff <= MIN_CLK_PER_HALFBIT;
-	else if(unlocked_for_longtime || (last_locked_ff && !locked)) begin
-		if(clk_per_halfbit_ff == MAX_CLK_PER_HALFBIT-1)
-			clk_per_halfbit_ff <= MIN_CLK_PER_HALFBIT;
-		else
-			clk_per_halfbit_ff <= clk_per_halfbit + 1;
-	end
-end
-assign clk_per_halfbit = clk_per_halfbit_ff;
-
-// clk_per_halfbit -> sampling rate
+reg [(NUM_RATE-1):0] rate_ff;
 parameter RATE_32 = 0;
 parameter RATE_441 = 1;
 parameter RATE_48 = 2;
 parameter RATE_96 = 3;
 parameter RATE_192 = 4;
-reg [(NUM_RATE):0] rate_ff;
 always @(posedge clk) begin
-    case (clk_per_halfbit)
-        5'd4, // rate: 192.0kHz
-        5'd5, // rate: 153.6kHz
-        5'd6: // rate: 128.0kHz
-            rate_ff <= 5'b1 << RATE_192;
-        5'd7, // rate: 109.7kHz
-        5'd8, // rate: 96.0kHz
-        5'd9, // rate: 85.3kHz
-        5'd10: // rate: 76.8kHz
-            rate_ff <= 5'b1 << RATE_96;
-        5'd11, // rate: 69.8kHz
-        5'd12, // rate: 64.0kHz
-        5'd13, // rate: 59.1kHz
-        5'd14, // rate: 54.9kHz
-        5'd15, // rate: 51.2kHz
-        5'd16: // rate: 48.0kHz
-            rate_ff <= 5'b1 << RATE_48;
-        5'd17, // rate: 45.2kHz
-        5'd18, // rate: 42.7kHz
-        5'd19, // rate: 40.4kHz
-        5'd20: // rate: 38.4kHz
-            rate_ff <= 5'b1 << RATE_441;
-        5'd21, // rate: 36.6kHz
-        5'd22, // rate: 34.9kHz
-        5'd23, // rate: 33.4kHz
-        5'd24, // rate: 32.0kHz
-        5'd25, // rate: 30.7kHz
-        5'd26, // rate: 29.5kHz
-        5'd27, // rate: 28.4kHz
-        5'd28, // rate: 27.4kHz
-        5'd29, // rate: 26.5kHz
-        5'd30, // rate: 25.6kHz
-        5'd31: // rate: 24.8kHz
-            rate_ff <= 5'b1 << RATE_32;
-        default:
-            rate_ff <= 0;
-    endcase
+	if (rst)
+		rate_ff <= 5'd1 << RATE_192;
+	else if(unlocked_for_longtime || (last_locked_ff && !locked)) begin
+		if(rate_ff == (5'd1 << RATE_32))
+            rate_ff <= 5'd1 << RATE_192;
+		else
+			rate_ff <= {1'b0, rate_ff[(NUM_RATE-1):1]}; // shr
+	end
 end
 assign rate_o = rate_ff;
+
+reg [(MAX_CLK_PER_HALFBIT_LOG2-1):0] clk_per_halfbit_ff;
+always @(posedge clk) begin
+    // sampling rate -> clk_per_halfbit
+    case (rate_ff)
+        5'd1 << RATE_32:
+            clk_per_halfbit_ff <= 5'd24;
+        5'd1 << RATE_441:
+            clk_per_halfbit_ff <= 5'd17; // or d18???
+        5'd1 << RATE_48:
+            clk_per_halfbit_ff <= 5'd16;
+        5'd1 << RATE_96:
+            clk_per_halfbit_ff <= 5'd8;
+        5'd1 << RATE_192:
+            clk_per_halfbit_ff <= 5'd4;
+        default:
+            clk_per_halfbit_ff <= 5'd4;
+    endcase
+end
+assign clk_per_halfbit = clk_per_halfbit_ff;
 
 endmodule
