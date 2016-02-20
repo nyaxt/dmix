6ef024f2af93383444ea8c5810a246c27f31527f
Kouhei Ueno <ueno@nyaxtstep.com>
2016-02-20 21:07:53 +0900

    led7

--- nkmm/arch/nkmm_progrom.v
+++ nkmm/arch/nkmm_progrom.v
@@ -6,23 +6,25 @@ module nkmm_progrom(
     output [`INSN_WIDTH-1:0] prog_data_o,
     input [`ADDR_WIDTH-1:0] prog_addr_i);
 
-reg [`INSN_WIDTH-1:0] out_ff;
+reg [`INSN_WIDTH-1:0] data_ff;
 
 always @(posedge clk) begin
     case (prog_addr_i)
+    // Insn{M[--] A OpAdd(R0, imm 7)}
+    16'h0000: data_ff <= 32'h04010007;
     // Insn{M[--] B OpAdd(R0, imm 2)}
-    16'h0001: out_ff <= 32'h08010002;
+    16'h0001: data_ff <= 32'h08010002;
     // Insn{M[--] C OpAdd(R0, imm 3)}
-    16'h0002: out_ff <= 32'h0c010003;
+    16'h0002: data_ff <= 32'h0c010003;
     // Insn{M[--] A OpAdd(B, C)}
-    16'h0003: out_ff <= 32'h04260000;
-    // Insn{M[W-] A OpAdd(R0, SP)}
-    16'h0004: out_ff <= 32'h440c0000;
-    default:
-        out_ff <= 32'h04260000;
+    16'h0003: data_ff <= 32'h04260000;
+    // Insn{M[W-] A OpAdd(R0, imm 61440)}
+    16'h0004: data_ff <= 32'h4401f000;
+    // Insn{M[--] D OpAdd(R0, imm 0)}
+    default: data_ff <= 32'h10010000;
     endcase
 end
 
-assign prog_data_o = out_ff;
+assign prog_data_o = data_ff;
 
 endmodule
