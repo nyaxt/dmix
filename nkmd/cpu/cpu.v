5ae71db80cea353c8c8fc4f7e58757a0acb13556
Kouhei Ueno <ueno@nyaxtstep.com>
2016-05-16 22:25:23 +0900

    compile pass

--- nkmd/cpu/cpu.v
+++ nkmd/cpu/cpu.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module nkmd_cpu_if(
     input clk,
     input rst,
@@ -9,7 +11,7 @@ module nkmd_cpu_if(
     output [31:0] inst_o);
 
 assign p_addr_o = next_pc_i;
-assign p_data_i = inst_o;
+assign inst_o = p_data_i ;
 
 endmodule
 
@@ -27,17 +29,7 @@ endmodule
 `define DCD_JMPREL_W 32 
 `define DCD_JMPREL (`DCD_IMM + `DCD_IMM_W)
 
-function [`DCD_WIDTH-1:0] nkmm_cpu_dcd_func(
-    input [31:0] inst_i);
-
-assign nkmm_cpu_dcd_func[`DCD_RSSEL+`DCD_REGSEL_W-1:`DCD_RSSEL] = inst_i[20:17];
-assign nkmm_cpu_dcd_func[`DCD_RTSEL+`DCD_REGSEL_W-1:`DCD_RTSEL] = inst_i[11:8];
-assign nkmm_cpu_dcd_func[`DCD_RDSEL+`DCD_REGSEL_W-1:`DCD_RDSEL] = inst_i[27:4];
-assign nkmm_cpu_dcd_func[`DCD_ALUSEL+`DCD_ALUSEL_W-1:`DCD_ALUSEL] = inst_i[23:21];
-assign nkmm_cpu_dcd_func[`DCD_IMM+`DCD_IMM_W-1:`DCD_IMM] = {17{inst_i[15]}, inst_i[14:0]};
-assign nkmm_cpu_dcd_func[`DCD_JMPREL+`DCD_JMPREL_W-1:`DCD_JMPREL] = {25{inst_i[15]}, inst_i[7:0]};
-
-endfunction
+`define DCD_WIDTH (`DCD_JMPREL + `DCD_JMPREL_W)
 
 module nkmd_cpu_dcd(
     input clk,
@@ -45,8 +37,43 @@ module nkmd_cpu_dcd(
 
     input [31:0] inst_i,
 
-    
+    output [`DCD_REGSEL_W-1:0] rssel_o,    
+    output [`DCD_REGSEL_W-1:0] rtsel_o,    
+    output [`DCD_REGSEL_W-1:0] rdsel_o,    
+    output [`DCD_ALUSEL_W-1:0] alusel_o,
+    output [`DCD_IMM_W-1:0] imm_o,
+    output [`DCD_JMPREL_W-1:0] jmprel_o
     );
+
+reg [`DCD_REGSEL_W-1:0] rssel_ff;
+reg [`DCD_REGSEL_W-1:0] rtsel_ff;
+reg [`DCD_REGSEL_W-1:0] rdsel_ff;
+reg [`DCD_ALUSEL_W-1:0] alusel_ff;
+reg [`DCD_IMM_W-1:0] imm_ff;
+reg [`DCD_JMPREL_W-1:0] jmprel_ff;
+
+function [`DCD_WIDTH-1:0] nkmd_cpu_dcd_func(
+    input [31:0] inst_i);
+begin
+    nkmd_cpu_dcd_func[`DCD_RSSEL+`DCD_REGSEL_W-1:`DCD_RSSEL] = inst_i[20:17];
+    nkmd_cpu_dcd_func[`DCD_RTSEL+`DCD_REGSEL_W-1:`DCD_RTSEL] = inst_i[11:8];
+    nkmd_cpu_dcd_func[`DCD_RDSEL+`DCD_REGSEL_W-1:`DCD_RDSEL] = inst_i[27:4];
+    nkmd_cpu_dcd_func[`DCD_ALUSEL+`DCD_ALUSEL_W-1:`DCD_ALUSEL] = inst_i[23:21];
+    nkmd_cpu_dcd_func[`DCD_IMM+`DCD_IMM_W-1:`DCD_IMM] = {{17{inst_i[15]}}, inst_i[14:0]};
+    nkmd_cpu_dcd_func[`DCD_JMPREL+`DCD_JMPREL_W-1:`DCD_JMPREL] = {{25{inst_i[15]}}, inst_i[7:0]};
+end
+endfunction
+
+always @(posedge clk) begin
+    {rssel_ff, rtsel_ff, rdsel_ff, alusel_ff, imm_ff, jmprel_ff} <= nkmd_cpu_dcd_func(inst_i);
+end
+
+assign rssel_o = rssel_ff;
+assign rtsel_o = rtsel_ff;
+assign rdsel_o = rdsel_ff;
+assign alusel_o = alusel_ff;
+assign imm_o = imm_ff;
+assign jmprel_o = jmprel_ff;
 
 endmodule
 
@@ -74,8 +101,12 @@ module nkmd_cpu(
 wire [31:0] if_dcd_inst;
 
 // DCD -> MEM
-wire [31:0] dcd_mem;
-
+wire [`DCD_REGSEL_W-1:0] dcd_mem_rssel;
+wire [`DCD_REGSEL_W-1:0] dcd_mem_rtsel;
+wire [`DCD_REGSEL_W-1:0] dcd_mem_rdsel;
+wire [`DCD_ALUSEL_W-1:0] dcd_mem_alusel;
+wire [`DCD_IMM_W-1:0] dcd_mem_imm;
+wire [`DCD_JMPREL_W-1:0] dcd_mem_jmprel;
 
 // WB -> IF
 wire [31:0] wb_if_next_pc;
@@ -83,7 +114,8 @@ wire [31:0] wb_if_next_pc;
 // IF: Instruction Fetch
 nkmd_cpu_if nkmd_cpu_if(
     .clk(clk), .rst(rst),
-    .p_data_i(p_data_i), .p_addr_o(p_addr_o)
+    .p_data_i(p_data_i),
+    .p_addr_o(p_addr_o),
     .next_pc_i(wb_if_next_pc),
     .inst_o(if_dcd_inst));
 
@@ -91,6 +123,11 @@ nkmd_cpu_if nkmd_cpu_if(
 nkmd_cpu_dcd nkmd_cpu_dcd(
     .clk(clk), .rst(rst),
     .inst_i(if_dcd_inst),
-    );
+    .rssel_o(dcd_mem_rssel),
+    .rtsel_o(dcd_mem_rtsel),
+    .rdsel_o(dcd_mem_rdsel),
+    .alusel_o(dcd_mem_alusel),
+    .imm_o(dcd_mem_imm),
+    .jmprel_o(dcd_mem_jmprel));
 
 endmodule
