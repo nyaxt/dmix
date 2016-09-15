507666f32b7dffe6972b6cc6cfc2a78212f704e3
Kouhei Ueno <ueno@nyaxtstep.com>
2016-09-16 08:09:52 +0900

    nkmd_dai_tx compiles

--- nkmd/arch/nkmd_dai.v
+++ nkmd/arch/nkmd_dai.v
@@ -12,18 +12,18 @@ module nkmd_dai_rx(
     input [31:0] addr_i,
     input we_i);
 
-reg [5:0] lastw_ff;
+reg [5:0] nextw_ff;
 always @(posedge clk) begin
     if (rst)
-        lastw_ff <= 0;
+        nextw_ff <= 0;
     else if (rx_ack_i)
-        lastw_ff <= lastw_ff + 1;
+        nextw_ff <= nextw_ff + 1;
 end
 
 reg [23:0] ringbuf [63:0];
 always @(posedge clk) begin
     if (rx_ack_i)
-        ringbuf[lastw_ff] <= rx_data_i;
+        ringbuf[nextw_ff] <= rx_data_i;
 end
 
 reg [5:0] unread_ff;
@@ -66,7 +66,6 @@ end
 
 endmodule
 
-/*
 module nkmd_dai_tx(
     input clk,
     input rst,
@@ -82,19 +81,62 @@ module nkmd_dai_tx(
     input [31:0] addr_i,
     input we_i);
 
+reg [5:0] queued_ff;
 reg [5:0] lastr_ff;
-always @(posedge clk) begin
-    if (rst)
-        lastr_ff <= 0;
-    else if (rx_pop_i)
-        lastr_ff <= lastr_ff + 1;
-end
+reg [5:0] nextw_ff;
 
 reg [23:0] ringbuf [63:0];
+assign tx_data_o = ringbuf[lastr_ff];
+
+wire should_queue;
+assign should_queue = we_i && addr_i[15:12] == 4'hd && addr_i[7:0] == 8'h01;
 always @(posedge clk) begin
-    if (rx_ack_i)
-        ringbuf[lastr_ff] <= rx_data_i;
+    if (rst) begin
+        queued_ff <= 0;
+        lastr_ff <= 0;
+        nextw_ff <= 0;
+    end else if (should_queue && !tx_pop_i) begin
+        ringbuf[nextw_ff] <= data_i;
+        queued_ff <= queued_ff + 1;
+        nextw_ff <= nextw_ff + 1;
+    end else if (!should_queue && tx_pop_i) begin
+        if (queued_ff > 0) begin
+            queued_ff <= queued_ff - 1;
+            lastr_ff <= lastr_ff + 1;
+        end
+    end else if (should_queue && tx_pop_i) begin
+        ringbuf[nextw_ff] <= data_i;
+        if (queued_ff > 0) begin
+            lastr_ff <= lastr_ff + 1;
+        end else begin
+            queued_ff <= queued_ff + 1;
+        end
+        nextw_ff <= nextw_ff + 1;
+    end
+end
+
+reg tx_ack_ff;
+always @(posedge clk)
+    tx_ack_ff <= tx_pop_i;
+
+// RAM interface
+reg [31:0] data_o_ff;
+assign data_o = data_o_ff;
+
+// {4'hd, ch[3:0], 8'h01} => tx ringbuf queued
+// {4'he, ch[3:0], offset} => tx ringbuf
+// FIXME: add ch check
+
+wire [5:0] offset_i;
+assign offset_i = addr_i[5:0];
+
+always @(posedge clk) begin
+    if (addr_i[15:12] == 4'he)
+        data_o_ff <= ringbuf[nextw_ff - offset_i];
+    else if (addr_i[15:12] == 4'hd && addr_i[7:0] == 8'h00)
+        data_o_ff <= queued_ff;
+    else
+        data_o_ff <= 0;
 end
 
 endmodule
-*/
