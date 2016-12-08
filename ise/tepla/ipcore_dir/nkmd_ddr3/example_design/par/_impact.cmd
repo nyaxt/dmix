981a6cf6b77b224855bacd8513e929dcfa32e1e0
Kouhei Ueno <ueno@nyaxtstep.com>
2016-12-08 18:53:00 +0900

    nkmd_ddr3 traffic gen working

---
+++ ise/tepla/ipcore_dir/nkmd_ddr3/example_design/par/_impact.cmd
@@ -0,0 +1,28 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify -inferir 
+Identify -inferir 
+identifyMPM 
+assignFile -p 1 -file "/home/kouhei/dmix/ise/tepla/ipcore_dir/nkmd_ddr3/example_design/par/example_top.bit"
+Program -p 1 
+Program -p 1 
+Program -p 1 
+Program -p 1 
+ReadStatusRegister -p 1 
+Program -p 1 
+ReadStatusRegister -p 1 
+Program -p 1 
+setMode -bs
+setMode -bs
+deleteDevice -position 1
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
